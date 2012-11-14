c3fda76c0c9e14fa4cdd6a382d5e3d0ac7d69049
Victor Lopata <v.lopata@webcerebrium.com>
2012-11-14 13:25:59 -0500

    set action for User_AccessList controller

--- classes/User/ctrl/AccessListCtrl.php
+++ classes/User/ctrl/AccessListCtrl.php
@@ -3,4 +3,31 @@
 class User_AccessListCtrl extends App_DbTableCtrl
 {
 
+    public function setAction()
+    {
+        $nResource = $this->_getParam( 'res_id', 0 );
+        $nRole = $this->_getParam( 'ucr_id',  0 );
+        $nEnable = $this->_getBoolParam( 'enable', 1 );
+        $this->_model = User_AccessList::Table();
+
+        $selectResult = $this->_model->select()
+                ->where( 'ucal_resource_id = ?', $nResource )
+                ->where( 'ucal_role_id = ?', $nRole );
+        $this->_object = $this->_model->fetchRow( $selectResult );
+        if ( !is_object( $this->_object ) ) {
+            if ( $nEnable ) {
+                $this->_object = $this->_model->createRow();
+                $this->_object->ucal_resource_id = $nResource;
+                $this->_object->ucal_role_id = $nRole;
+                $this->_object->save();
+            }
+        } else {
+            if ( ! $nEnable ) {
+                $this->_object->delete();
+                $this->_object = null;
+            }
+        }
+
+        $this->view->object = $this->_object;
+    }
 }
