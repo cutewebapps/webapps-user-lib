7a1ddf856981cf6f5aa5203b63a88bc68945dc1f
Victor Lopata <v.lopata@webcerebrium.com>
2013-03-22 06:57:03 -0400

    user role must be unique on editing

--- classes/User/ctrl/RoleCtrl.php
+++ classes/User/ctrl/RoleCtrl.php
@@ -20,4 +20,30 @@ class User_RoleCtrl extends App_DbTableCtrl
         }
         parent::getlistAction();
     }
+    
+    public function editAction()
+    {
+        if ( $this->_isPost() && ! $this->_getParam( 'ucr_id' ) ) {
+            
+            $arrErrors = array();
+            
+            // if we are adding a new record, prevent adding with the same login
+            $strNewRole = $this->_getParam( 'ucr_name' );
+            if ( $strNewRole != "" ) {
+                $objUser = User_Role::Table()->findByName( $strNewRole );
+                if ( is_object( $objUser ) ) {
+                    $arrErrors[] = 'Role with such name already exists'; 
+                }
+            }
+            
+            if ( count( $arrErrors ) > 0 ) {
+                $this->view->lstErrors = $arrErrors;
+                $this->view->object = User_Role::Table()->createRow();
+                return;
+            }
+        }
+  
+        //add role if it is provided
+        parent::editAction();
+    }
 }
